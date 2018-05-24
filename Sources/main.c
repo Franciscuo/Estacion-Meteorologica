15e1e5eede48e435152a549abdd50033641543c8
nickdpz <ndpastranz@correo.udistrital.edu.co>
2018-05-23 21:22:43 -0500

    Se termina proyecto

--- Sources/main.c
+++ Sources/main.c
@@ -7,12 +7,13 @@ unsigned int i,j;//VARIABLES PARA QUE FUNCIONEN LAS SURUTINAS
 const unsigned char Temperatura[17]={"TEMPERATURA:   C"};
 const unsigned char Presion[17]	= 	{"PRESION:     KPA"};
 const unsigned char Humedad[17]	= 	{"HUMEDAD:       %"};
-const unsigned char lluvia[17]	= 	{"LLUVIA:         "};
+const unsigned char lluvia[17]	= 	{"LLUVIA:    CM3/S"};
 const unsigned char Viento1[17]	= 	{"VELOCIDAD DE    "};
 const unsigned char Viento2[17]	= 	{"VIENTO:     KM/H"};
 volatile unsigned char cont = 0;
 volatile unsigned long temp=0;
-volatile unsigned int aux=0,hume=0,velo=0,pre=0,lluvi=0;
+volatile unsigned long aux=0,hume=0,velo=0,pre=0,lluvi=0;
+volatile unsigned char aux2=0;
 //----------RUTINAS LCD-----------------------
 //--------------------------------------------
 
@@ -50,9 +51,9 @@ void act_dato(unsigned long help,unsigned char punto){
 	vtemp=(unsigned int)help%100;
 	Dhelp=vtemp/10;
 	Uhelp=vtemp%10;
-	//cursor(punto);
-	//PTED=Chelp+0x30;
-	//dato();
+	cursor(punto);
+	PTED=Chelp+0x30;
+	dato();
 	cursor(punto+1);
 	PTED=Dhelp+0x30;
 	dato();
@@ -82,11 +83,13 @@ void	act_msj(){
 			PTED=Presion[j];
 			dato();
 		}
+		act_dato(pre,0b10001001);
 		cursor(0b11000000);
 		for(j=0;j<sizeof(lluvia)-1;j++){
 			PTED=lluvia[j];
 			dato();
 		}
+		act_dato(lluvi,0b11001000);
 	}else{
 		cursor(0b10000000);
 		for(j=0;j<sizeof(Viento1)-1;j++){
@@ -98,6 +101,7 @@ void	act_msj(){
 			PTED=Viento2[j];
 			dato();
 		}
+		act_dato(velo,0b11001000);
 	}
 		
 }
@@ -136,6 +140,9 @@ void main(void) {
 		PTED=0b00000001;
 		comando();
 		tiempo(20000);
+	//----------------Configura SPI-------------------------------
+		SCI2BD=13;//9600 
+		SCI2C2_RE=1;//habilita
 	//------------------------------------------------------------	
 		//---------Configuracion Timer 1-----------
 		TPM2SC = 0b01000111;
@@ -143,12 +150,15 @@ void main(void) {
 		TPM2SC_CLKSA = 1;	 //Habilita Timer 2
 		EnableInterrupts;
   for(;;) {
+	  //ENVIO DE DATOS AL TRANSMISO
   } 
 
 }  
  
 interrupt VectorNumber_Vtpm2ovf void TPM2_ISR(void){
   	TPM2SC_TOF=0;
+  	//while(SCI2S1_RDRF==0);//Espera a que se llene el registro (LLega completa informacion)
+  	//pre=SCI2D;//Se obtiene el dato recibido
   	if(cont==0){
   		ADCSC1=0b0000000;//Inicia conversion con canal 0- Temperatura
   		while(ADCSC1_COCO==0);//Pregunta si ya finalizo la conversion, NO SE ACTUALIZA BIT POR QE DESPUES SE SOBRE ESCRIBE
@@ -157,30 +167,33 @@ interrupt VectorNumber_Vtpm2ovf void TPM2_ISR(void){
   		ADCSC1=0b0000001;//Inicia conversion con canal 1- Humedad
   		while(ADCSC1_COCO==0);//Pregunta si ya finalizo la conversion, NO SE ACTUALIZA BIT POR QE DESPUES SE SOBRE ESCRIBE
   		aux=ADCR;//Lee dato de la conversion
-  		hume=(500*aux)/1023;//conversiones
+  		hume=(120*aux)/1023;//conversiones
   		act_msj();
   		cont=cont+1;
   	}
   	else if(cont==1){
-  		ADCSC1=0b0000010;//Inicia conversion con canal 2- presion
-  		while(ADCSC1_COCO==0);//Pregunta si ya finalizo la conversion, NO SE ACTUALIZA BIT POR QE DESPUES SE SOBRE ESCRIBE
-  		aux=ADCR;//Lee dato de la conversion
-  		pre=(500*aux)/1023;//conversiones
+  		//ADCSC1=0b0000010;//Inicia conversion con canal 2- presion
+  	  	while(SCI2S1_RDRF==0);//Espera a que se llene el registro (LLega completa informacion)
+  	  	pre=SCI2D;//Se obtiene el dato recibido
+  	  	pre=(pre*75)/221;
   		ADCSC1=0b0000011;//Inicia conversion con canal 3- cantidad de lluvia
   		while(ADCSC1_COCO==0);//Pregunta si ya finalizo la conversion, NO SE ACTUALIZA BIT POR QE DESPUES SE SOBRE ESCRIBE
   		aux=ADCR;//Lee dato de la conversion
-  		lluvi=(500*aux)/1023;//conversiones
+  		lluvi=50-(50*aux)/1023-;//conversiones
+
+  		
   		act_msj();
   		cont=cont+1;
   	}
   	else{
-  		ADCSC1=0b0000100;//Inicia conversion con canal 4- cantidad de lluvia
+  		ADCSC1=0b0000010;//Inicia conversion con canal 4- cantidad de lluvia
   		while(ADCSC1_COCO==0);//Pregunta si ya finalizo la conversion, NO SE ACTUALIZA BIT POR QE DESPUES SE SOBRE ESCRIBE
   		aux=ADCR;//Lee dato de la conversion
-  		velo=(500*aux)/1023;//conversiones
+  		velo=(10*aux)/1023;//conversiones
   		act_msj();
   		cont=0;
   	}
+  	
   }
   
   
